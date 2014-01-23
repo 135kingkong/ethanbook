92b1630324de5f49e5384ead2b0b50c0fa355e05
Ethan Hill <ethanblues.10@gmail.com>
2014-01-23 17:30:53 +0000

    Styled Flash Messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def flash_class(type)
+    case type
+    when :alert
+      "alert-danger"
+    when :notice
+      "alert-success"
+    else 
+      "alert-info"
+    end
+  end  
+  
 end
