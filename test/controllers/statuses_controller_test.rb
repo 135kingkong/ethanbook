6911943b0903520e67c126565a7febd247047fd8
Ethan Hill <ethanblues.10@gmail.com>
2014-01-24 13:01:55 +0000

    Added status validations and test validations

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -23,7 +23,15 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:ethan)
+    
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -36,12 +44,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should be logged in to get edit" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+  
+  test "should get edit when logged in" do
+    sign_in users(:ethan)
     get :edit, id: @status
     assert_response :success
   end
+  
+  test "should redirect status update when not logged in" do
+    patch :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 
-  test "should update status" do
+  test "should update status when logged in" do
+    sign_in users(:ethan)
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
