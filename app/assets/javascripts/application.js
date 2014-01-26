fe07a5d421667d5f44a816a6578246863ff82448
Ethan Hill <ethanblues.10@gmail.com>
2014-01-26 09:42:10 +0000

    Added jquery migrate again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,12 @@
 // about supported directives.
 //
 //= require jquery
+
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require jquery-migrate-1.2.1
+//= require bootstrap
+
 
 //= require turbolinks
 //= require_tree .
-//= require bootstrap
+
